0eb3c18b70f41b64b8e2fe7d005ccbb17c223d06
Justin Lewis <justinmichlewis@gmail.com>
2025-07-12 19:30:28 -0700

    Update shitty min max function, need to refactor

--- src/utils/chart.ts
+++ src/utils/chart.ts
@@ -1,8 +1,16 @@
 export const getMixMaxTempRange = (data: any[]) => {
   if (!data || data.length === 0) return [40, 100];
   const temperatures = data.map((d) => d.temperature);
-  const minTemp = (Math.floor(Math.min(...temperatures) * 10) / 10).toFixed(0);
-  const maxTemp = (Math.ceil(Math.max(...temperatures) * 10) / 10).toFixed(0);
+  const historicalTemperatures = data.map((d) => d.historicalTemperature);
+  let minTemp = Math.floor(Math.min(...temperatures) * 10) / 10;
+  let maxTemp = Math.ceil(Math.max(...temperatures) * 10) / 10;
+  const historicalMinTemp =
+    Math.floor(Math.min(...historicalTemperatures) * 10) / 10 - 1;
+  const historicalMaxTemp =
+    Math.ceil(Math.max(...historicalTemperatures) * 10) / 10 + 1;
+
+  if (historicalMinTemp < minTemp) minTemp = historicalMinTemp;
+  if (historicalMaxTemp > maxTemp) maxTemp = historicalMaxTemp;
 
   console.log("Min Temp:", minTemp, "Max Temp:", maxTemp);
   return [minTemp, maxTemp];
